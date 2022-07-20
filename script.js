4911bf9a5743038245b8b51bc11470316dd595fd
Jeff <farlandjeff@gmail.com>
2022-07-20 13:59:18 -0400

    Implement drawing functionality

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const sketchBoard = document.querySelector('.board');
+let draw = false;
 
+window.addEventListener('mouseup', () => {
+    draw = false;
+})
 
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 16 * 16; i++) {
     let div = document.createElement('div');
+    div.addEventListener('mousedown', (e) => {
+        draw = true;
+        e.target.style.backgroundColor = 'black';
+    });
+    div.addEventListener('mouseover', changeColor);
     sketchBoard.appendChild(div);
+}
+
+function changeColor(e) {
+    if (draw == false) return
+    else {
+        e.target.style.backgroundColor = 'black';
+    }
 }
